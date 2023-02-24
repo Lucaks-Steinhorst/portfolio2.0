df522893ed1175b5f64bd94a8c1de57316e64019
Fillipeaugusto <fillipeaugustoribeiro@gmail.com>
2023-02-24 01:48:36 -0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./src/**/*.tsx'],
+	theme: {
+		extend: {
+			fontFamily: {
+				sans: 'Roboto',
+				poppins: 'Poppins',
+			},
+		},
+	},
+	plugins: [],
+};
